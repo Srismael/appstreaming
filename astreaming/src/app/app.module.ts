0e418bf89ace60adaaa011a392d760a48181f081
ismael <ismaelmtzballin@gmail.com>
2024-03-13 20:04:32 -0600

    implementacion de registrar usuario

--- astreaming/src/app/app.module.ts
+++ astreaming/src/app/app.module.ts
@@ -9,6 +9,19 @@ import { UserComponent } from './view/user/user.component';
 import { HomeComponent } from './view/home/home.component';
 import { SignupComponent } from './view/signup/signup.component';
 import { MiComponent } from './view/mi/mi.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes =[
+  {path: '', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'sigup', component: SignupComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'mi', component: MiComponent}
+
+]
+
+
 
 @NgModule({
   declarations: [
@@ -22,9 +35,13 @@ import { MiComponent } from './view/mi/mi.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- astreaming/src/app/view/signup/signup.component.ts
+++ astreaming/src/app/view/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/model/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  nuevoUsuario: Usuario = {
+    nombre_usuario: '',
+    correo: '',
+    contrasenia: '',
+    pelicula_vistas: [],
+    foto: '',
+    listas: []
+  };
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  registrarUsuario(): void {
+    this.usuarioService.agregarUsuario(this.nuevoUsuario)
+      .subscribe(() => {
+        console.log('Usuario registrado exitosamente');
+        // Puedes agregar aquí redirección a otra página o cualquier otra lógica que desees después de registrar el usuario
+      }, error => {
+        console.error('Error al registrar usuario:', error);
+      });
+  }
 
 }
